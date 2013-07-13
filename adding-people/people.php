a480f356b5e1b80eab15c33cbec3590dc7b17c51
Andy Bell <andy@lazyrivr.com>
2013-07-13 10:19:31 -0600

    Finishing touches
    I think I'm just about done with everything I want to do with this!

--- adding-people/people.php
+++ adding-people/people.php
@@ -15,8 +15,43 @@
 				<li><a href="#nigelhancock"><img src="http://files.andy-bell.com/310/images/nigelhancock.jpg" alt="Nigel Hancock" /></a></li>
 			</ul>
 		</nav>
-		<section class="profile" id="cordelldurand">
-		<!--content goes here-->
+		<section class='profile' id='cordelldurand'>
+			<ul class='profile-banner'>
+				<li><img src='http://files.andy-bell.com/310sample/cordelldurand/310/images/cdpic1.jpg' alt='This is one photo.' /></li>
+				<li><img src='http://files.andy-bell.com/310sample/cordelldurand/310/images/pic2.jpg' alt='This is a second photo.' /></li>
+				<li><img src='http://files.andy-bell.com/310sample/cordelldurand/310/images/pic3cd.jpg' alt='This is a third photo.' /></li>
+			</ul>
+			<ul class='profile-social'>
+				<li class='twitter'><a href='http://twitter.com/lazyrivr'>Twitter</a></li>
+				<li class='facebook'><a href='http://facebook.com'>Facebook</a></li>
+				<li class='blog'><a href='http://www.andy-bell.com/blog/'>Blog</a></li>
+				<li class='youtube'><a href='http://youtube.com/user/lazyrivr'>YouTube</a></li>
+				<li class='mormon'><a href='http://mormon.org'>Mormon.org</a></li>
+				<li class='website'><a href='http://files.andy-bell.com/310sample/cordelldurand'>Website</a></li>
+			</ul>
+			<img src='http://files.andy-bell.com/310sample/cordelldurand/310/images/cordelldurand.jpg' class='profile-pic' alt='Cordell Durand' />
+			<h1>Cordell Durand</h1>
+			<h2>playing bass</h2>
+			<aside class='profile-topten'>
+				<h3>Favorite Songs to Play Bass On</h3>
+				<ul>
+					<li>One Song</li>
+					<li>Two Song</li>
+					<li>Three Song</li>
+					<li>Four Song</li>
+					<li>Five Song</li>
+					<li>Six Song</li>
+					<li>Seven Song</li>
+					<li>Eight Song</li>
+					<li>Nine Song</li>
+					<li>Ten Song</li>
+				</ul>
+			</aside>
+			<p>This is one paragraph. Well, it's not really a paragraph, but this is a sample anyway.</p>
+			<p>So I guess this is another one.</p>
+			<p>And this is a third one.</p>
+			<h2>PHP-izzle!</h2>
+			<p>I learned how to do PHP in this class. Yay!</p>
 		</section>
 <?php
 
